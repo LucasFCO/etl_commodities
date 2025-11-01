dafc5b5f141011ac295487d83402857b52130efc
Lucas Oliveira <lucaowar@gmail.com>
2025-10-31 21:34:11 -0300

    criação do bd e init do dbt

---
+++ src/extract_load.py
@@ -0,0 +1,14 @@
+# import
+import pandas as pd
+import yfinance as yf
+from sqlalchemy import create_engine
+from dotenv import load_dotenv 
+import os
+
+# import variaveis de ambiente
+
+# Extract
+
+# Concatena os dados extraidos
+
+# Salva os dados no BD
